181a5ec138a684d6fbb139e12d7adcd5ff46c281
Yannic <r4zzeryt@gmail.com>
2026-01-12 22:16:58 +0100

    created basic angular project (SSR, tailwind, JEST)

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
+};
+export default config;
