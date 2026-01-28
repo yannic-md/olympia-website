91ce95c83d60072098e29c1537b2ed6e8d68d9f1
Yannic <r4zzeryt@gmail.com>
2026-01-28 09:15:01 +0100

    Added all necessary site routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {DetailedComponent} from "./pages/detailed/detailed.component";
+import {LoginComponent} from "./pages/login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'detailed', component: DetailedComponent },
+  { path: '**', component: HomeComponent }
+];
---
+++ src/app/pages/detailed/detailed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed',
+  imports: [],
+  templateUrl: './detailed.component.html',
+  styleUrl: './detailed.component.css',
+})
+export class DetailedComponent {
+
+}
