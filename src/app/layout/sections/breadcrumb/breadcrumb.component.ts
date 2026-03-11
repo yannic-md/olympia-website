611996e14119312619e8ea9d54c433b7611d0451
Yannic <r4zzeryt@gmail.com>
2026-03-11 17:05:52 +0100

    feature: added slide Animation for breadcrumb section

--- src/app/layout/sections/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/sections/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {isPlatformBrowser} from "@angular/common";
 import {MiscService} from "../../../services/misc/misc.service";
 import {TranslatePipe} from "@ngx-translate/core";
 
@@ -8,11 +9,19 @@ import {TranslatePipe} from "@ngx-translate/core";
   imports: [
     RouterLink,
     RouterLinkActive,
-    TranslatePipe
+    TranslatePipe,
   ],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css',
 })
 export class BreadcrumbComponent {
-  constructor(protected router: Router, protected miscService: MiscService) {}
+  protected skipEntryAnimation: boolean = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              protected router: Router, protected miscService: MiscService) {
+    if (isPlatformBrowser(this.platformId)) {
+      // Don't replay the animation after an in-app route change – only on the initial page load.
+      this.skipEntryAnimation = document.readyState === 'complete';
+    }
+  }
 }
